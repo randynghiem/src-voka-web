1cb097d43231e5e22349ed01c0e94864be19f981
randynghiem <randy.nghiem@hotmail.com>
2020-01-12 13:30:13 +0000

    Deploying to gh-pages from  4f6b5d065e8b76bb95e760acac3e2ff103f5f0aa

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/src-voka-web/gh-pages/precache-manifest.c5c8f9c77879aff037cc08066999c184.js"
+  "/src-voka-web/precache-manifest.cf888be04cd1d7afcf30757c46421514.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/src-voka-web/gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/src-voka-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
